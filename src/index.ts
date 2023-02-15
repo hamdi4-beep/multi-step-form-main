cc0e16038da51bb32b088217ed9b4f6363e49908
Hamdi Kamel <hamdi.pirillo4@gmail.com>
2023-02-15 11:14:52 -0800

    fixing deployment issues

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express, { Application, Request, Response } from 'express'
 
 const app: Application = express()
+const PORT = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 app.use(express.static('../public'))
 
-app.listen(process.env.PORT || 3000)
+app.listen(PORT)
 
 app.get('/', (req: Request, res: Response): void => res.render('index.ejs'))
 app.get('/plans', (req: Request, res: Response): void => res.render('pages/plans.ejs'))
