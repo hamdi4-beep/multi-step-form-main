5d7dbe128fa65971a265cd28c460ee8f62a1988f
Hamdi Kamel <hamdi.pirillo4@gmail.com>
2023-02-15 12:58:20 -0800

    modified package.json

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app: Application = express()
 const PORT = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname + '/views'))
+app.set('views', path.join('../views'))
 
 console.log(path.join(__dirname, '/views'))
 
