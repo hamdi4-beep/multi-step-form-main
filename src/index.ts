5c8b096de49db331dc8e924ab0f1f7bdee033f06
Hamdi Kamel <hamdi.pirillo4@gmail.com>
2023-02-15 09:39:31 -0800

    fixed some production issues with deployment

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import express, {Application, Request, Response} from 'express'
+import express, { Application, Request, Response } from 'express'
 
 const app: Application = express()
 
 app.set('view engine', 'ejs')
 app.use(express.static('../public'))
 
-app.listen(process.env.PORT || 8000)
+console.log('Running')
+
+app.listen(process.env.PORT || 3000)
 
 app.get('/', (req: Request, res: Response): void => res.render('index.ejs'))
 app.get('/plans', (req: Request, res: Response): void => res.render('pages/plans.ejs'))
