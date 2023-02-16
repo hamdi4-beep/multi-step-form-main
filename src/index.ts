74972eeea90c8af189135fa79c5cc1695f2fd096
Hamdi Kamel <hamdi.pirillo4@gmail.com>
2023-02-16 09:54:42 -0800

    working on the addons section

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, { Application, Request, Response } from 'express'
 import path from 'path'
 
 const app: Application = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8000
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, '../public')))
 
 app.listen(PORT)
 
-app.get('/', (req: Request, res: Response): void => res.render('index.ejs'))
-app.get('/plans', (req: Request, res: Response): void => res.render('pages/plans.ejs'))
-app.get('/addons', (req: Request, res: Response): void => res.render('pages/addons.ejs'))
-app.get('/confirmation', (req: Request, res: Response): void => res.render('pages/confirmation.ejs'))
+app.get('/', (req: Request, res: Response): void => res.render('index'))
+app.get('/plans', (req: Request, res: Response): void => res.render('pages/plans'))
+app.get('/addons', (req: Request, res: Response): void => res.render('pages/addons'))
+app.get('/confirmation', (req: Request, res: Response): void => res.render('pages/confirmation'))
