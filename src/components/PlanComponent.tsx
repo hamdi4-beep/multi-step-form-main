ebacb20a5276df0216211da5cd223dbddfc74768
hamdi <hamdi.pirillo4@gmail.com>
2024-11-18 05:49:03 +0100

    prevented the page from moving to the next step when the current plan is empty

--- src/components/PlanComponent.tsx
+++ src/components/PlanComponent.tsx
@@ -32,11 +32,11 @@ const plans = [{
 
 function PlanComponent() {
     const [currentPlan, setCurrentPlan] = React.useState('')
-    const [toggleValue, setToggleValue] = React.useState('mo')
+    const [subscription, setSubscription] = React.useState('mo')
     const location = useLocation()
 
     const togglerStyle = `
-        ${toggleValue === 'yr' ? 'toggled' : ''}
+        ${subscription === 'yr' ? 'toggled' : ''}
         after:block after:bg-white after:rounded-full after:py-[.04em] after:px-[.6em] after:w-4 after:h-5
         bg-primary-marine-blue p-1 rounded-full w-14
     `
@@ -52,28 +52,28 @@ function PlanComponent() {
                         <div onClick={() => setCurrentPlan(plan.title)} className={`${currentPlan === plan.title ? 'selected-plan' : ''} w-full mt-8 p-4 pr-16 border rounded-md hover:selected-plan`} key={i}>
                             <img src={plan.src} className='mb-10' alt="" />
                             <h3 className="font-bold text-primary-marine-blue">{plan.title}</h3>
-                            <p className="text-neutral-cool-gray">{plan.price[toggleValue]}</p>
+                            <p className="text-neutral-cool-gray">{plan.price[subscription]}</p>
                         </div>
                     )
                 })}
             </div>
 
             <div className="bg-[#eee] p-2 flex gap-4 justify-center mt-8 items-center rounded-md">
-                <span className={`${toggleValue === 'yr' ? 'text-neutral-cool-gray' : 'text-primary-marine-blue font-bold'}`}>Monthly</span>
-                <div onClick={() => setToggleValue(prev => prev === 'yr' ? 'mo' : 'yr')} className={togglerStyle}></div>
-                <span className={`${toggleValue === 'mo' ? 'text-neutral-cool-gray' : 'text-primary-marine-blue font-bold'}`}>Yearly</span>
+                <span className={`${subscription === 'yr' ? 'text-neutral-cool-gray' : 'text-primary-marine-blue font-bold'}`}>Monthly</span>
+                <div onClick={() => setSubscription(prev => prev === 'yr' ? 'mo' : 'yr')} className={togglerStyle}></div>
+                <span className={`${subscription === 'mo' ? 'text-neutral-cool-gray' : 'text-primary-marine-blue font-bold'}`}>Yearly</span>
             </div>
 
             <div className="flex justify-between items-center mt-32">
                 <button className="text-neutral-cool-gray">
-                    <Link to='/'>Go Back</Link>
+                    <Link to='..'>Go Back</Link>
                 </button>
 
                 <button className="primary-btn">
-                    <Link to='/add-ons' state={{
+                    <Link to={currentPlan === '' ? '#' : '/add-ons'} onClick={() => currentPlan === '' ? alert('Pick a plan!') : null} state={{
                         ...location.state,
                         plan: currentPlan,
-                        subscription: toggleValue
+                        subscription
                     }}>Next Step</Link>
                 </button>
             </div>
